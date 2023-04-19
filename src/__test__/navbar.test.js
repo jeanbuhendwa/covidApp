b12a95ce5f4ac0a5200bf084ba8eb023554666d0
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-19 21:58:48 +0300

    Add testing

---
+++ src/__test__/navbar.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "../components/navbar";
+import "@testing-library/jest-dom";
+import { useLocation } from "react-router-dom";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: jest.fn(),
+}));
+
+describe("NavBar component", () => {
+  let mockUseLocation;
+
+  beforeEach(() => {
+    mockUseLocation = useLocation.mockImplementation(() => ({
+      pathname: "/some/path",
+    }));
+  });
+
+  afterEach(() => {
+    mockUseLocation.mockRestore();
+  });
+
+  test("renders without errors", () => {
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    );
+  });
+
+  test("does not render the back button when not on country page", () => {
+    const mockLocation = { pathname: "/" };
+    mockUseLocation.mockReturnValue(mockLocation);
+
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    );
+
+    expect(screen.queryByText("<")).toBeNull();
+  });
+});
