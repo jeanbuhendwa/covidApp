59c87284db538729df44da023dde03ce49692d86
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-19 11:03:01 +0300

    Update homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 import FetchStats from '../store/api';
 import { GetStats } from '../store/reducer';
 
@@ -15,30 +17,40 @@ const HomePage = () => {
   }, [CountryStore.length, dispatch]);
 
   const Africa = CountryStore.filter((item) => item.continent === 'Africa');
-
+  const header = 'Get Covid Stats in Africa';
   return (
-    <ul className="dataUL">
-      {Africa.map((country) => (
-        <Link
-          key={country.country}
-          to={{ pathname: `/country/${country.country}` }}
-        >
-          <li className="countryDetails">
-            <div>
-              {country.country}
-              <br />
-              Population:
-              {' '}
-              {country.population}
-            </div>
-            <div>
-              <img src={country.country_flag} alt="flag" className="flag" />
-            </div>
-          </li>
-        </Link>
-      ))}
-    </ul>
+    <>
+      <div className="branding">
+        <h1 className="brand-text">{header}</h1>
+      </div>
+      <ul className="countries-container">
+        {Africa.map((country) => (
+          <Link
+            key={country.country}
+            to={{ pathname: `/country/${country.country}` }}
+          >
+            <li className="countryDetails">
+              <div>
+                <img
+                  src={country.country_flag}
+                  alt="flag"
+                  className="country-flag"
+                />
+              </div>
+
+              <div>
+                <h2 className="country-title">{country.country}</h2>
+                <p className="country-pop">
+                  <FontAwesomeIcon icon={faPeopleGroup} />
+                  {' '}
+                  <span className="pop-span">{country.population}</span>
+                </p>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </>
   );
 };
-
 export default HomePage;
